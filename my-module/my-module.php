a88954844a391a1ac2c89764f8bc661696fdc20f
Govinda Dahal <govindahal05@gmail.com>
2023-10-02 12:37:46 +0545

    work in module setting

--- my-module/my-module.php
+++ my-module/my-module.php
@@ -1,19 +1,40 @@
 <?php
 class MyModuleClass extends FLBuilderModule {
 
-public function __construct()
-{
-  parent::__construct(array(
-    'name'            => __( 'My Module', 'fl-builder' ),
-    'description'     => __( 'A totally awesome module!', 'fl-builder' ),
-    'group'           => __( 'My Group', 'fl-builder' ),
-    'category'        => __( 'My Category', 'fl-builder' ),
-    'dir'             => MY_MODULES_DIR . 'my-module/',
-    'url'             => MY_MODULES_URL . 'my-module/',
-    'icon'            => 'button.svg',
-    'editor_export'   => true, // Defaults to true and can be omitted.
-    'enabled'         => true, // Defaults to true and can be omitted.
-    'partial_refresh' => false, // Defaults to false and can be omitted.
-  ));
+    public function __construct()
+    {
+        parent::__construct(
+            array(
+            'name'            => __( 'My Module', 'fl-builder' ),
+            'description'     => __( 'A totally awesome module!', 'fl-builder' ),
+            'group'           => __( 'My Group', 'fl-builder' ),
+            'category'        => __( 'My Category', 'fl-builder' ),
+            'dir'             => CMB_DIR . 'my-module/',
+            'url'             => CMB_URL . 'my-module/',
+            'icon'            => 'button.svg',
+            'editor_export'   => true, // Defaults to true and can be omitted.
+            'enabled'         => true, // Defaults to true and can be omitted.
+            'partial_refresh' => false, // Defaults to false and can be omitted.
+        ));
+    }
 }
-}
+FLBuilder::register_module( 'MyModuleClass', array(
+    'my-tab-1'      => array(
+      'title'         => __( 'Tab 1', 'fl-builder' ),
+      'sections'      => array(
+        'my-section-1'  => array(
+          'title'         => __( 'Section 1', 'fl-builder' ),
+          'fields'        => array(
+            'my-field-1'     => array(
+              'type'          => 'text',
+              'label'         => __( 'Text Field 1', 'fl-builder' ),
+            ),
+            'my-field-2'     => array(
+              'type'          => 'text',
+              'label'         => __( 'Text Field 2', 'fl-builder' ),
+            )
+          )
+        )
+      )
+    )
+  ) );
