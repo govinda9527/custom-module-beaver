5b5a12e1faba4e804a3c74207d6ab4578bc5d3e1
Govinda Dahal <govindahal05@gmail.com>
2023-10-02 10:45:41 +0545

    plugin initialize

--- custom-module-beaver.php
+++ custom-module-beaver.php
@@ -7,12 +7,12 @@
  * Author: Govinda
  * Author URI: http://www.govindahal.com.np
  */
-define( 'MY_MODULES_DIR', plugin_dir_path( __FILE__ ) );
-define( 'MY_MODULES_URL', plugins_url( '/', __FILE__ ) );
+define( 'CMB_DIR', plugin_dir_path( __FILE__ ) );
+define( 'CMB_URL', plugins_url( '/', __FILE__ ) );
 
-function my_load_module_examples() {
+function my_load_module_one() {
   if ( class_exists( 'FLBuilder' ) ) {
       // Include your custom modules here.
   }
 }
-add_action( 'init', 'my_load_module_examples' );
+add_action( 'init', 'my_load_module_one' );
